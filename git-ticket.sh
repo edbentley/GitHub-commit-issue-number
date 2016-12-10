8e3074ba5ad976a2516aff67b3876891b60176ad
Edward Bentley <edward@factual.com>
2016-12-10 16:16:16 +0800

    add remove ticket

--- git-ticket.sh
+++ git-ticket.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-if [ -z $1 ]; then
-  READING=true
-else
+READING=true
+REMOVING=false
+
+if [[ $1 ]]; then
   READING=false
+  if [ $1 = "rm" ]; then
+    REMOVING=true
+  elif [[ ! $1 =~ ^[0-9]+$ ]]; then
+    echo "Ticket number must be an integer"
+    exit
+  fi
 fi
 
 touch .git/hooks/branch-tickets.map
@@ -14,19 +21,22 @@ BRANCH_MAP=`cat .git/hooks/branch-tickets.map`
 regex="(.*)$BRANCH_NAME ([0-9]+)(.*)"
 if [[ $BRANCH_MAP =~ $regex ]]
 then
-  if [ "$READING" = true ]; then
-    echo "Current branch ticket is ${BASH_REMATCH[2]}"
+  if [ "$REMOVING" = true ]; then
+    echo "${BASH_REMATCH[1]}${BASH_REMATCH[3]}" > .git/hooks/branch-tickets.map
+    echo "Removed branch ticket number"
+  elif [ "$READING" = true ]; then
+    echo "Current branch ticket number is ${BASH_REMATCH[2]}"
   else
     echo "${BASH_REMATCH[1]}$BRANCH_NAME $1${BASH_REMATCH[3]}" > .git/hooks/branch-tickets.map
   fi
 else
-  if [ "$READING" = true ]; then
-    echo "No ticket set for current branch"
+  if [ "$READING" = true ] || [ "$REMOVING" = true ]; then
+    echo "No ticket number set for current branch"
   else
     echo "$BRANCH_NAME $1" >> .git/hooks/branch-tickets.map
   fi
 fi
 
-if [ $READING = false ]; then
-  echo "Set current branch ticket number to $1"
+if [ $READING = false ] && [ "$REMOVING" = false ]; then
+  echo "Current branch ticket number set to $1"
 fi
