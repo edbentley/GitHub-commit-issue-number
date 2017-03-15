fb2221be47addd0ed5c799a860fbcd466c7ffb22
Edward Bentley <edward@factual.com>
2017-03-15 11:27:39 +0800

    echo commands during setup

--- setup.sh
+++ setup.sh
@@ -7,17 +7,29 @@ GIT_TICKET_PATH="$DIR/git-ticket.sh"
 echo "export GIT_TICKET_PATH=\"$GIT_TICKET_PATH\"" >> $HOME/.bashrc
 echo "export GIT_TICKET_PATH=\"$GIT_TICKET_PATH\"" >> $HOME/.bash_profile
 
+echo "Added env var GIT_TICKET_PATH to .bashrc and .bash_profile"
+
 # add git ticket to config
 git config --global alias.ticket '!sh $GIT_TICKET_PATH $1'
+
+echo "Added git ticket alias to .gitconfig"
 
 # create .git_template dir
 if [ ! -d "$HOME/.git_template" ]; then
   mkdir -p $HOME/.git_template/hooks
+  echo "Made .git_template dir"
 fi
 
 # symlink `prepare-commit-msg` and `branch-ticket-restrictions.sh`
-ln -s $DIR/prepare-commit-msg $HOME/.git_template/hooks/prepare-commit-msg
-ln -s $DIR/branch-ticket-restrictions.sh $HOME/.git_template/hooks/branch-ticket-restrictions.sh
+ln -sf $DIR/prepare-commit-msg $HOME/.git_template/hooks/prepare-commit-msg
+ln -sf $DIR/branch-ticket-restrictions.sh $HOME/.git_template/hooks/branch-ticket-restrictions.sh
+
+echo "Symlink prepare-commit-msg and branch-ticket-restrictions.sh into .git_template dir"
 
 # setup git template
 git config --global init.templatedir '~/.git_template'
+
+echo "Set up git template destination in .gitconfig"
+
+echo "-------"
+echo "All done! Just run 'source ~/.bashrc' and start using git ticket in your repos"
